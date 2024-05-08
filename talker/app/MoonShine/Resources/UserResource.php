349cb5a90909b5debdbf4653f569a2700cf8d5ac
gamletel <gamletel@list.ru>
2024-05-08 10:02:54 +0300

    Add moonshine

---
+++ talker/app/MoonShine/Resources/UserResource.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MoonShine\Resources;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+use MoonShine\Resources\ModelResource;
+use MoonShine\Decorations\Block;
+use MoonShine\Fields\ID;
+use MoonShine\Fields\Text;
+use MoonShine\Fields\Field;
+use MoonShine\Components\MoonShineComponent;
+
+/**
+ * @extends ModelResource<User>
+ */
+class UserResource extends ModelResource
+{
+    protected string $model = User::class;
+
+    protected string $title = 'Пользователи';
+
+    protected string $column = 'id';
+
+    protected bool $createInModal = true;
+
+    protected bool $editInModal = true;
+    protected bool $detailInModal = true;
+
+    /**
+     * @return list<MoonShineComponent|Field>
+     */
+    public function fields(): array
+    {
+        return [
+            Block::make([
+                ID::make()->sortable(),
+                Text::make('Имя', 'name'),
+                Text::make('Email', 'email'),
+            ]),
+        ];
+    }
+
+    /**
+     * @param User $item
+     *
+     * @return array<string, string[]|string>
+     * @see https://laravel.com/docs/validation#available-validation-rules
+     */
+    public function rules(Model $item): array
+    {
+        return [];
+    }
+}
